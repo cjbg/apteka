fd8f1d865f75980e0021ff61872d71580bed814d
karol chudzik <charles.wd40@gmail.com>
2013-04-16 23:19:50 +0200

    takie tam zmiany

---
+++ Apteka/Controllers/DiscountsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apteka.Controllers
+{
+    public class DiscountsController : Controller
+    {
+        //
+        // GET: /Discounts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

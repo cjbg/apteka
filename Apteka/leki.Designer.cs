d4de8d48a660017c89c499d6b75cfb0ec3f4d35c
jczabator <jczabator@gmail.com>
2013-05-15 13:32:49 +0200

    zmiany

--- Apteka/leki.Designer.cs
+++ Apteka/leki.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Andre\Documents\GitHub\apteka\Apteka\leki.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\cj\Documents\GitHub\Apteka\Apteka\leki.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
